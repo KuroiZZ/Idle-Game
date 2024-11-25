456c879b6f1a1174dd342a24786a570f433750b7
KuroiZZ <habiltatar01@gmail.com>
2024-11-25 22:29:10 +0300

    Increasing system has changed.

--- src/LOCSystem/LOC.java
+++ src/LOCSystem/LOC.java
@@ -12,10 +12,12 @@ public class LOC //Line of Code
     public int loc_cnt; //LOC counter
     public LOC.LOCHandler loc_hndler = new LOC.LOCHandler();
     public Timer autoLOCIncreaser;
+    Beginner C_Beginner = new Beginner();
 
     public static void main(String[] args)
     {
         new LOC(); //creates a new LOC object
+
     }
 
     public LOC()//constructor
@@ -39,17 +41,21 @@ public class LOC //Line of Code
                     setAutoLOCIncreaser(1000); //1000 millisecond = 1 second
                     autoLOCIncreaser.start();
                     break;
+                case "C_Beginner":
+                    setAutoLOCIncreaser(C_Beginner.LinePerSecond);
+                    autoLOCIncreaser.start();
             }
         }
     }
 
-    public void setAutoLOCIncreaser(int delayMS)//LOC is increased by 1 every "delayMS" millisecond
+    public void setAutoLOCIncreaser(int LinePerSecond)//LOC is increased by 1 every "delayMS" millisecond
     {
-        autoLOCIncreaser = new Timer(delayMS, new ActionListener() {
+        autoLOCIncreaser = new Timer(1000, new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                loc_cnt++;
+                loc_cnt += LinePerSecond;
                 SoftvoperMain.LOCLabel.setText("Number of LOC: "+loc_cnt);
             }
         });
