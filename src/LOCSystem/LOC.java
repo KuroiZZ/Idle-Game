8b809fd30506952ca92b6c77646599c264767fc7
Güneş Balcı <gunesbalci66@hotmail.com>
2024-11-28 00:25:32 +0300

    Maindeki bir kaç satır fonksiyon olarak taşındı.
    main.java'daki alt satırlardaki kodlar bir fonksiyona dönüştürüldü ve LOC.java'ya taşındı.
    LOC.java'daki bir kaç yazım hatası düzeltildi.

--- src/LOCSystem/LOC.java
+++ src/LOCSystem/LOC.java
@@ -11,14 +11,18 @@ public class LOC //Line of Code
 {
     public int loc_cnt; //LOC counter
     public LOC.LOCHandler loc_hndler = new LOC.LOCHandler();
-    public Beginner C_Beginner = new Beginner();
-    public Beginner Beginner_CSharp = new Beginner();
-    public Beginner Beginner_Dart = new Beginner();
-    public Beginner Beginner_Java = new Beginner();
+    public Beginner C_Beginner;
+    public Beginner Beginner_CSharp;
+    public Beginner Beginner_Dart;
+    public Beginner Beginner_Java;
 
     public LOC()//constructor
     {
         loc_cnt = 0;
+        C_Beginner = new Beginner();
+        Beginner_CSharp = new Beginner();
+        Beginner_Dart = new Beginner();
+        Beginner_Java = new Beginner();
     }
 
     public class LOCHandler implements ActionListener //handles events of LOC objects
@@ -33,19 +37,34 @@ public class LOC //Line of Code
                     loc_cnt++;
                     break;
                 case "C_Beginner":
-                    C_Beginner.NofTotalEmp++;
+                    C_Beginner.setNTEandNNPEandNTL(C_Beginner.getNofTotalEmp()+1);
                     break;
                 case "Beginner_CSharp":
-                    Beginner_CSharp.NofTotalEmp++;
+                    Beginner_CSharp.setNTEandNNPEandNTL(Beginner_CSharp.getNofTotalEmp()+1);
                     break;
                 case "Beginner_Dart":
-                    Beginner_Dart.NofTotalEmp++;
+                    Beginner_Dart.setNTEandNNPEandNTL(Beginner_Dart.getNofTotalEmp()+1);
                     break;
                 case "Beginner_Java":
-                    Beginner_Java.NofTotalEmp++;
+                    Beginner_Java.setNTEandNNPEandNTL(Beginner_Java.getNofTotalEmp()+1);
                     break;
 
             }
         }
+    }
+    public void UpdateLOC()
+    {
+        Timer timer = new Timer(1000, new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                loc_cnt += C_Beginner.getNofTotalLOC() + Beginner_CSharp.getNofTotalLOC();
+                loc_cnt += Beginner_Dart.getNofTotalLOC() + Beginner_Java.getNofTotalLOC();
+
+                SoftvoperMain.LOCLabel.setText("Number of LOC: "+ loc_cnt);
+            }
+        });
+        timer.start();
     }
 }
