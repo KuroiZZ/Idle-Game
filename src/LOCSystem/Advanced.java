bdee70d5e144791287e237932f75a6c76b6de9e1
KuroiZZ <habiltatar01@gmail.com>
2024-11-28 19:14:34 +0300

    New developer subclasses created.

---
+++ src/LOCSystem/Advanced.java
@@ -0,0 +1,11 @@
+package LOCSystem;
+
+public class Advanced extends Developers
+{
+    public Advanced()
+    {
+        super();
+        setPrice(20);
+        setLinePerSecond(10000);
+    }
+}
---
+++ src/LOCSystem/Intermediate.java
@@ -0,0 +1,11 @@
+package LOCSystem;
+
+public class Intermediate extends Developers
+{
+    public Intermediate()
+    {
+        super();
+        setPrice(10);
+        setLinePerSecond(5000);
+    }
+}
