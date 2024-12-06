ff089ba86b529022c6c6c83eda9c7eeca269f900
KuroiZZ <habiltatar01@gmail.com>
2024-12-06 13:15:45 +0300

    now CreateUI used like static function.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import GUI.SoftvoperMain;
 import LOCSystem.LOC;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args)
     {
-        SoftvoperMain UI = new SoftvoperMain();
-        UI.CreateUI();
+        SoftvoperMain.CreateUI();
 
         LOC.UpdateLOC();
     }
