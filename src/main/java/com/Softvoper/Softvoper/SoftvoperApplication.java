0b61809e525fa923f7755089d99ec2ae2e19b0bb
KuroiZZ <habiltatar01@gmail.com>
2024-12-09 16:45:49 +0300

    API project has started.

---
+++ src/main/java/com/Softvoper/Softvoper/SoftvoperApplication.java
@@ -0,0 +1,14 @@
+package com.Softvoper.Softvoper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftvoperApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SoftvoperApplication.class, args);
+	}
+
+}
