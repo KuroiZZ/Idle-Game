6db559a9fc5830c6ff419d4466a5e24ad962c281
KuroiZZ <habiltatar01@gmail.com>
2024-12-12 05:15:33 +0300

    Creating game without save and with save actions added.

--- src/Handlers/GUI_Handler.java
+++ src/Handlers/GUI_Handler.java
@@ -3,10 +3,18 @@ package Handlers;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import GUI.GUI_Elements;
 import GUI.SoftvoperMain;
-
-import javax.swing.*;
+import LOCSystem.Developers;
+import LOCSystem.LOC;
+import SaveSystem.Save;
+import SaveSystem.SaveSystem;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class GUI_Handler implements ActionListener
 {
@@ -38,6 +46,47 @@ public class GUI_Handler implements ActionListener
                 GUI_Elements.window.revalidate();
                 GUI_Elements.window.repaint();
                 break;
+            case "CreateGame_New":
+                GUI_Elements.window.getContentPane().removeAll();
+
+                List<Developers> Developers = LOC.CreateEmptyDevelopers();
+                SoftvoperMain.CreateGameMenu();
+                Save save = new Save("Araba", Developers);
+                try
+                {
+                    SaveSystem.SendSave(save.CreateJSON());
+                }
+                catch (JsonProcessingException e)
+                {
+                    throw new RuntimeException(e);
+                }
+
+                GUI_Elements.window.revalidate();
+                GUI_Elements.window.repaint();
+                LOC.UpdateLOC();
+                break;
+            case "CreateGame_Old":
+                GUI_Elements.window.getContentPane().removeAll();
+                String saveJson = SaveSystem.GetSave();
+                String[] contents = SaveSystem.ParseJsonString(saveJson);
+
+                Pattern pattern = Pattern.compile("\\{[^}]*\\}");
+                Matcher matcher = pattern.matcher(contents[4]);
+
+                ArrayList<String> developer_strings = new ArrayList<>();
+
+                while (matcher.find())
+                {
+                    developer_strings.add(matcher.group());
+                }
+
+                List<Developers> developers = LOC.CreateSavedDevelopers(developer_strings);
+                SoftvoperMain.CreateGameMenu();
+                Save save_new = new Save(contents[0], contents[1], Integer.parseInt(contents[2]), Integer.parseInt(contents[3]), developers);
+                GUI_Elements.window.revalidate();
+                GUI_Elements.window.repaint();
+                LOC.UpdateLOC();
+
         }
     }
 
