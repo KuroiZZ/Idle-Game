bd20206bf5bdd15554050ecdfe514377a5047683
KuroiZZ <habiltatar01@gmail.com>
2024-12-13 19:06:17 +0300

    JsonCreator constructor created. GetTotalPrice function modified. SetNTEandNNPE function added.

--- src/LOCSystem/Supporter.java
+++ src/LOCSystem/Supporter.java
@@ -1,5 +1,8 @@
 package LOCSystem;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Supporter extends Employee
 {
     public Supporter(String Type, float Price)
@@ -8,6 +11,18 @@ public class Supporter extends Employee
         setNofTotalEmp(0);
         setNofProjectEmp(0);
         setNofNonProjectEmp();
+    }
+
+    @JsonCreator
+    public Supporter(@JsonProperty("price") float Price, @JsonProperty("type") String Type,
+                     @JsonProperty("nofNonProjectEmp") int NofNonProjectEmp, @JsonProperty("nofTotalEmp") int NofTotalEmp,
+                     @JsonProperty("nofProjectEmp") int NofProjectEmp)
+
+    {
+        super(Type, Price);
+        this.NofTotalEmp = NofTotalEmp;
+        this.NofProjectEmp = NofProjectEmp;
+        this.NofNonProjectEmp = NofNonProjectEmp;
     }
 
     @Override
@@ -20,7 +35,22 @@ public class Supporter extends Employee
     }
 
     @Override
-    public float getTotalPrice(int BuyAmount) {
-        return 0;
+    public float getTotalPrice(int BuyAmount)
+    {
+        float TotalPrice = 0;
+        float ActivePrice = getPrice();
+        for (int i = 0; i<BuyAmount; i++)
+        {
+            TotalPrice += ActivePrice;
+            ActivePrice *= 1.05f;
+        }
+        return TotalPrice;
     }
+
+    public void setNTEandNNPE(int nofTotalEmp)
+    {
+        this.NofTotalEmp = nofTotalEmp;
+        setNofNonProjectEmp();
+    }
+
 }
